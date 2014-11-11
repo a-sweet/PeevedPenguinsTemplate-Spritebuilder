25c1323b547cd183b764d49659f7af199c0a2076
Andrew Sweet <andrewsweet@MBPR.local>
2014-11-11 09:09:02 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -31,7 +31,7 @@ static const float MIN_SPEED = 5.f;
     self.userInteractionEnabled = TRUE;
     
     // visualize physics bodies & joints
-    _physicsNode.debugDraw = TRUE;
+    //_physicsNode.debugDraw = TRUE;
     
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
